1d192051e167d46c1706b7b403023236af612a2f
taydeo <td12233a@gmail.com>
2021-11-02 16:10:55 -0500

    added left right movement!

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController _controller;
+    public float speed = 3.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get player movement from a, d
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        
+        // always multi by Time.deltaTime
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
