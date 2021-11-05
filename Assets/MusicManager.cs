dfdc289e8f8d840579339afaf232ddba603f665f
taydeo <td12233a@gmail.com>
2021-11-05 17:47:10 -0500

    cleanup

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 /// <summary>
 ///     Global audio object, use for soundtracks and stuff.
