a9552fefa9b5f68d6b498200fa43b498ac1c7fd4
Jake <93555663+jak0vitz@users.noreply.github.com>
2021-11-09 13:25:40 -0600

    Revert "Revert "Merge branch 'master' of https://github.com/CAT-Dawgs/gameoff2021""
    This reverts commit 8e396a669067af00c2838bb9591f3a15732d8c8b.

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+/// <summary>
+///     Global audio object, use for soundtracks and stuff.
+/// </summary>
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    
+    private void Start()
+    {
+        _audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public AudioSource GetAudioSource()
+    {
+        return _audioSource;
+    }
+
+    public void SetAudioClip(AudioClip audioClip)
+    {
+        _audioSource.clip = audioClip;
+    }
+}
