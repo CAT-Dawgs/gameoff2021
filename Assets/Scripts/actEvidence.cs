22c3cc5d05d64b60f53aac87ff021f0a3b4b35e9
SIUKnupp <tyler.knupp@siu.edu>
2021-11-19 23:44:34 -0600

    script comments
    add comments to actEvidence script

--- Assets/Scripts/actEvidence.cs
+++ Assets/Scripts/actEvidence.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+/*
+This script allows the player to press the "e" key
+when they overlap an interactable evidence object.
+When e is pressed another set of objects will appear,
+one of which is the evidence to collect and will be clickable.
+THIS IS NOT FINISHED AS OF Nov-19
+*/
 public class actEvidence : MonoBehaviour
 {
+	//variable for hitbox collide, interact buttone is pressed,
+	//and for other "evidence" object
 	public bool collide = false;
 	public bool isPressed = false;
 	public GameObject evidenceObject;
 	
+	//disable evidence object from play space
 	private void Start(){
 		evidenceObject.active = false;
 	}
 	
     private void Update(){
+		//check if "e" is pressed
 		if(Input.GetKeyDown(KeyCode.E)){
 			if(collide == true){
 				isPressed = true;
@@ -27,13 +37,16 @@ public class actEvidence : MonoBehaviour
 		}
 	}
 	
+	//enables evidence object
 	void showEvidence(){
 			evidenceObject.active = true;
 	}
+	//disables evidence object
 	void hideEvidence(){
 			evidenceObject.active = false;
 	}
 	
+	//activates when player hitbox collides with interactable object
 	void OnTriggerEnter2D(Collider2D other){
 		collide = true;
 		if(Input.GetKeyDown(KeyCode.E)){
@@ -45,10 +58,9 @@ public class actEvidence : MonoBehaviour
 		}
 	}
 	
+	//activated when hitboxes no longer collide
 	void OnTriggerExit2D(Collider2D other){
-		//Debug.Log("Trigger Exit");
 		collide = false;
-		//Debug.Log(collide);
 		isPressed = false;
 		evidenceObject.active = false;
 	}
