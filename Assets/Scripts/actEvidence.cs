22756a971ffb4ea5f4e9d1362570872cf8832a58
SIUKnupp <tyler.knupp@siu.edu>
2021-11-19 23:24:32 -0600

    add evidence interract
    while standing on interactable, press e, evidence image will popup.  TO ADD: Clickable object that is evidence.

--- Assets/Scripts/actEvidence.cs
+++ Assets/Scripts/actEvidence.cs
@@ -6,29 +6,51 @@ using UnityEngine.UI;
 
 public class actEvidence : MonoBehaviour
 {
-	
+	public bool collide = false;
 	public bool isPressed = false;
+	public GameObject evidenceObject;
+	
+	private void Start(){
+		evidenceObject.active = false;
+	}
 	
     private void Update(){
 		if(Input.GetKeyDown(KeyCode.E)){
-			isPressed = true;
+			if(collide == true){
+				isPressed = true;
+				showEvidence();
+			}
 		}
-		if(Input.GetKeyUp(KeyCode.E)){
+		if(collide == false || Input.GetKeyUp(KeyCode.E)){
 			isPressed = false;
+			hideEvidence();
 		}
+	}
 	
+	void showEvidence(){
+			evidenceObject.active = true;
+	}
+	void hideEvidence(){
+			evidenceObject.active = false;
 	}
 	
 	void OnTriggerEnter2D(Collider2D other){
+		collide = true;
 		if(Input.GetKeyDown(KeyCode.E)){
 			isPressed = true;
 		}
 		if(Input.GetKeyUp(KeyCode.E)){
 			isPressed = false;
+			evidenceObject.active = false;
 		}
 	}
 	
 	void OnTriggerExit2D(Collider2D other){
+		//Debug.Log("Trigger Exit");
+		collide = false;
+		//Debug.Log(collide);
 		isPressed = false;
+		evidenceObject.active = false;
 	}
+	
 }
